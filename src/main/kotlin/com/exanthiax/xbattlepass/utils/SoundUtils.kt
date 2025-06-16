d3bce486aa645e4d6de88ef4523ebee7f2099989
Exanthiax <107284021+Exanthiax@users.noreply.github.com>
2025-06-16 21:58:41 +0100

    SoundFixees
    - Moved close-button location in battlepass-gui to row 3
    - Updated SoundUtils to use Eco’s built-in SoundUtils for improved cross-version Minecraft support

--- src/main/kotlin/com/exanthiax/xbattlepass/utils/SoundUtils.kt
+++ src/main/kotlin/com/exanthiax/xbattlepass/utils/SoundUtils.kt
@@ -1,17 +1,21 @@
 package com.exanthiax.xbattlepass.utils
 
 import com.exanthiax.xbattlepass.plugin
-import org.bukkit.Sound
+import com.willfp.eco.util.SoundUtils as EcoSoundUtils
 import org.bukkit.entity.Player
 
 object SoundUtils {
     fun playIfEnabled(player: Player, configPath: String) {
+        val sound = EcoSoundUtils.getSound(plugin.configYml.getFormattedString("$configPath.sound")) ?: return
+        val pitch = plugin.configYml.getDoubleFromExpression("$configPath.pitch")
+        val volume = plugin.configYml.getDoubleFromExpression("$configPath.volume")
+
         if (plugin.configYml.getBool("$configPath.enabled")) {
             player.playSound(
                 player.location,
-                Sound.valueOf(plugin.configYml.getString("$configPath.sound").uppercase()),
-                plugin.configYml.getDouble("$configPath.volume").toFloat(),
-                plugin.configYml.getDouble("$configPath.pitch").toFloat()
+                sound,
+                volume.toFloat(),
+                pitch.toFloat()
             )
         }
     }
